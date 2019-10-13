d1f9d572c8b264198d4d049497e748a3c30750ad
Yuriy17 <yuriy_sh@zoho.com>
2019-10-13 04:08:52 +0300

    repair-design-project/feat: config webpack

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [{
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: false
+                    },
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/i,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        outputPath: "assets/fonts"
+                    }
+                }]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        outputPath: "assets/img"
+                    }
+                }]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id]'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
+};
